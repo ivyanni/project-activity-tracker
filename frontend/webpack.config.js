5cb67bc22b844d06822f11fef772569adba0bb85
Ilia Vianni <ivyanni@outlook.com>
2017-12-02 18:41:27 +0300

    [#3] Add babel support

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,17 +3,34 @@ var path = require('path');
 var webpack = require('webpack');
 
 const PATHS = {
-    build: path.join(__dirname, 'target', 'classes', 'META-INF', 'resources', 'webjars', packageJSON.name, packageJSON.version),
-    dev: path.join(__dirname, 'dist')
+    build: path.join(__dirname, 'target', 'classes', 'META-INF', 'resources', 'webjars', packageJSON.name, packageJSON.version)
 };
 
 module.exports = {
-    entry: './app/index.js',
+    entry: ['babel-polyfill', './app/index.js'],
     devtool: 'cheap-module-inline-source-map',
 
     devServer: {
         port: 9000,
         historyApiFallback: false
+    },
+
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+                include: [
+                    path.resolve(__dirname, "app")
+                ],
+                exclude: [
+                    path.resolve(__dirname, "node_modules")
+                ],
+                test: /\.jsx?$/,
+                query: {
+                    presets: ['env', 'react']
+                }
+            }
+        ]
     },
 
     output: {
